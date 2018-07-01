85ed088d4200c0e5afc76fc062bc7af2d928d764
Jim Cooke <Jim.Cooke@coogee.email>
2018-07-01 16:47:12 +1000

    testing mods

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,69 +1,48 @@
 extern crate tdigest;
 
 use std::fs::File;
-use std::io::prelude::*;
 use std::io::BufReader;
 use tdigest::Tdigest;
 
 #[test]
 fn integation_test_save_centroids() {
-    let result: i32;
     let mut t = Tdigest::new(1000.0);
     t.add(469.20, 1.0);
     match t.save_centroids("tests/centroid_test.json".to_string()) {
-        Ok(_) => {
-            result = 0;
-        }
+        Ok(_) => (),
         Err(_err) => {
-            result = 1;
+            panic!("Failed to save centroid");
         }
     }
-    assert_eq!(result, 0);
 }
 
-fn integration_test_digest(fspec: String) {
-    let result: i32;
-    let f = File::open(&fspec).expect("file");
-    let f = BufReader::new(f);
+fn integration_test_digest(fname: String) {
+    // Load the statistics file ... the test 'answers', if you will, generated with R
+    let fspec = format!("{}.sta", fname);
+    let f = File::open(&fspec);
+    let f = match f {
+        Ok(file) => file,
+        Err(e) => {
+            panic!(
+                "Failed to open dataset statistics check file {}: {}",
+                fspec, e
+            );
+        }
+    };
 
-    for line in f.lines() {
-        println!("{}", line.unwrap());
-    }
-    result = 0;
-    assert_eq!(result, 0);
+    let mut fileReader = BufReader::new(File::open(&path));
+    //    for line in fileReader.lines() {
+    //       match line {
+    //         Err(_why)   => { result = 0; },
+    //         Ok(string) => match string.trim().parse::<f64>() {
+    //            None        => panic!("Not a number!"),
+    //           Some(number)=> { t.add(number, 1.0); count += 1; }
+    //      }
+    //     }
+    //    }
 }
-//    let result: i32 = 0;
-//    let mut fileReader = BufReader::new(File::open(&fspec));
-//    let mut vector: Vec<isize> = vec![];
-//
-//    for line in fileReader.Lines() {
-//        match line {
-//            Err(why) => {
-//                println!("{:?}", why);
-//                result = 1;
-//            }
-//            Ok(string) => match string.trim().parse::<isize>() {
-//                None => {
-//                    println!("Not a number!");
-//                    result = 1;
-//                }
-//                Some(number) => println!("{}", number),
-//            },
-//        }
-//    }
 
 #[test]
 fn integation_test_digests_with_datasets() {
-    integration_test_digest("data/lrg-skew-dataset.dat".to_string());
+    integration_test_digest("data/large-normal".to_string());
 }
-//
-//    let mut fileReader = BufReader::new(File::open(&path));
-//    for line in fileReader.lines() {
-//       match line {
-//         Err(_why)   => { result = 0; },
-//         Ok(string) => match string.trim().parse::<f64>() {
-//            None        => panic!("Not a number!"),
-//           Some(number)=> { t.add(number, 1.0); count += 1; }
-//      }
-//     }
-//    }
