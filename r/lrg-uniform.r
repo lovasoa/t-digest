cedd1c86f697debf157c29c41fcdf84ab4cd61b8
ophir <pere.jobs@gmail.com>
2018-07-26 21:19:37 +0200

    remove huge R tests

--- r/lrg-uniform.r
+++
@@ -1,6 +0,0 @@
-u
-<- rnorm(100000)*100
-p <- c(0,5, 1, 5, 25, 50, 75, 95, 99, 99.5)/100
-a <- quantile(u,p)
-write(u, file="~/src/t-digest/data/large-normal.dat", ncolumns=1)
-write(a, file="~/src/t-digest/data/large-normal.sta", ncolumns=1)
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -8,35 +8,6 @@ use std::io::BufReader;
 use tdigest::Tdigest;
 
 #[test]
-fn integration_test_save_centroids() {
-    let mut t = Tdigest::new(1000.0);
-    for i in 0..10 {println!("{}", i)};
-    t.add_weighted(469.20, 1.0);
-    match t.save_centroids("tests/centroid_test.json".to_string()) {
-        Ok(_) => (),
-        Err(_err) => {
-            panic!("Failed to save centroid");
-        }
-    }
-}
-
-fn integration_test_digest(fname: String) {
-    // Load the statistics file ... the test 'answers', if you will, generated with R
-    let fspec = format!("{}.sta", fname);
-    let f = File::open(&fspec);
-    let f = match f {
-        Ok(file) => file,
-        Err(e) => panic!("Failed to open dataset statistics check file {}: {}", fspec, e)
-    };
-}
-
-#[test]
-fn integation_test_digests_with_datasets() {
-    integration_test_digest("data/large-normal".to_string());
-}
-
-
-#[test]
 fn linear() {
     let mut t = Tdigest::new(1000.0);
     let max = 100000;
