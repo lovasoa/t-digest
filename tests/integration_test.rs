9d351bd1ae4a15078a04a2d8015f8e03f52e5d87
ophir <pere.jobs@gmail.com>
2018-07-27 00:09:19 +0200

    add quickcheck tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -3,7 +3,11 @@ extern crate tdigest;
 #[macro_use]
 extern crate assert_approx_eq;
 
+#[macro_use]
+extern crate quickcheck;
+
 use tdigest::Tdigest;
+use std::cmp::{max, min};
 
 #[test]
 fn linear() {
@@ -14,4 +18,31 @@ fn linear() {
     };
     assert_approx_eq!(max as f64/2.0, t.quantile(0.5), 100.);
     assert_approx_eq!(max as f64/10.0, t.quantile(0.1), 100.);
+}
+
+#[test]
+fn empty() {
+    assert!(Tdigest::new(1000.0).quantile(0.1).is_nan());
+}
+
+fn median_tdigest(xs: &Vec<f64>) -> f64 {
+    let mut t = Tdigest::new(1000.0);
+    for &x in xs {
+        t.add(x);
+    };
+    t.quantile(0.5)
+}
+
+fn is_valid_median(xs: &Vec<f64>, median: f64) -> bool {
+    let less = xs.iter().filter(|&&x| x <= median).count();
+    let more = xs.iter().filter(|&&x| x >= median).count();
+
+    min(less, more) <= 2 * max(less, more)
+}
+
+#[cfg(test)]
+quickcheck! {
+  fn test_valid_median(xs: Vec<f64>) -> bool {
+      is_valid_median(&xs, median_tdigest(&xs))
+  }
 }
